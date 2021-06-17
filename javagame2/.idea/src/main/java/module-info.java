98f79f296c8f7af5bf4baedf86134396ae703c66
avacyrus <79265105+avacyrus@users.noreply.github.com>
2021-06-17 21:41:00 +0430

    Delete javagame2 directory

--- javagame2/.idea/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module vis {
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires com.google.gson;
-    opens Sample to javafx.fxml;
-          exports Sample;
-}
